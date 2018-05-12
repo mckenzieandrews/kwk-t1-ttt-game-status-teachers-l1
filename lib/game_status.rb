2a5e8b5d84fd781a2dca048301ba6a35af952db4
McKenzie Andrews <c.mckenzie.a@gmail.com>
2018-05-12 22:30:49 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,9 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5]  # Middle row
+  [6,7,8]
+  # ETC, an array for each win combination
+]
